bf284885aaaab1f1f9e60561ece9d6a69f8b64ff
IO <48241519+IOxee@users.noreply.github.com>
2023-09-09 18:37:51 +0200

    Add Angular Modifications

--- typescript/src/web/src/app/app.component.ts
+++ typescript/src/web/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NUIService } from './services/nui.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'web';
+	title = 'Fractal Game Studios - ' + (window as any).GetParentResourceName(); // Get the resource name from the server
+
+	visible: boolean = false;
+	constructor(private NUI: NUIService) {}
+
+	ngOnInit(): void {
+		this.NUI.fromMessageAction<boolean>('setVisible').subscribe((visible: boolean) => {
+			next: (value: boolean) => {
+				this.visible = value;
+			}
+		});
+
+		this.NUI.dispatchDebugMessages([
+			{
+				action: 'setVisible',
+				data: true
+			}
+		]);
+	}
+
+	@HostListener('document:keydown.escape', ['$event'])
+	handleKeyboardEvent(event: KeyboardEvent) {
+		if (['Backspace', 'Escape'].includes(event.code)) {
+			if (!this.NUI.isEnvBrowser()) this.NUI.fetchNUI('hideFrame');
+			this.visible = false;
+		}
+	}
 }
--- typescript/src/web/src/app/app.module.ts
+++ typescript/src/web/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module'; // Enable this if you want to use Angular routing in your app
 import { AppComponent } from './app.component';
 
+/* ======================== CUSTOM COMPONENTS ======================= */
+//import { ExampleComponent } from './components/example/example.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		//ExampleComponent
+	],
+	imports: [
+		BrowserModule,
+		//AppRoutingModule // Enable this if you want to use Angular routing in your app
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
