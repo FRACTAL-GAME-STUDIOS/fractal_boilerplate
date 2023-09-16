137c0d6f7fc6301571519d082d0631b4f6dfa0d5
IO <48241519+IOxee@users.noreply.github.com>
2023-09-16 14:19:02 +0200

    FXManifest and package modfiicactions

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,11 @@ lua54 'yes'
 
 
 client_scripts {
-    'client/*.js',
+    'dist/client/*.js',
 }
 
 server_scripts {
-    'server/*.js',
+    'dist/server/*.js',
 }
 
 ui_page "dist/web/index.html"
